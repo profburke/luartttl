03b5f5ec6f1378ae9cdea129e072f87abfc3f8cb
John Mark <john.mark@kistler.com>
2025-02-20 09:55:21 -0500

    refactor rtttl to pull out player

--- play.lua
+++ play.lua
@@ -1,9 +1,10 @@
 #!/usr/bin/env lua
 
 local rtttl = require 'rtttl'
+local play = require('sox-player').play
 
 local infile = io.open(arg[1], 'r')
 local m = infile:read('a')
 local rt = rtttl.parse(m)
 
-rtttl.play(rt)
+play(rt)
